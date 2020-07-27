8cf2920e98dc869cabeb34a639c8e2d9f62d5eaf
M Dek <marek.deja@yahoo.com>
2020-07-27 13:24:02 +0200

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -20,19 +20,32 @@ router.post('/signup', function (req, res) {
       res.json({ err: err })
     }
     else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json')
-        res.json({ success: true, status: 'Registration successful' })
+      if (req.body.firstname)
+        user.firstname = req.body.firstname
+      if (req.body.lastname)
+        user.lastname = req.body.lastname
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json')
+          res.json({ err: err })
+          return
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json')
+          res.json({ success: true, status: 'Registration successful' })
+        })
       })
+
     }
   })
 
 })
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  
-  var token = authenticate.getToken({_id: req.user._id})
+
+  var token = authenticate.getToken({ _id: req.user._id })
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json')
   res.json({ success: true, token: token, status: 'You are successfully logged in!' })
