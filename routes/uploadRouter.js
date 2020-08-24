c286731ba47fbcc2c57f8093f2ccb0a2112bdd8b
M Dek <marek.deja@yahoo.com>
2020-08-24 15:33:59 +0200

    Uploading files

---
+++ routes/uploadRouter.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const authenticate = require('../authenticate')
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const imageFileFilter = (req, file, cb) => {
+    //added case sensitive
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('You can upload only image files!'), false)
+    }
+    cb(null, true)
+}
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter })
+
+const uploadRouter = express.Router()
+
+uploadRouter.use(bodyParser.json())
+
+uploadRouter.route('/')
+    .get(authenticate.verifyOrdinaryUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('GET operation not suported on /imageUpload')
+    })
+    //TODO: Why only small files are accepted? in postman: Error: read ECONNRESET
+    .post(authenticate.verifyOrdinaryUser, authenticate.verifyAdmin,
+        upload.single('imageFile'), (req, res, next) => {
+            console.log(req)
+            console.log(res)
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json')
+            res.json(req.file)
+
+        })
+    .put(authenticate.verifyOrdinaryUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not suported on /imageUpload')
+    })
+    .delete(authenticate.verifyOrdinaryUser, authenticate.verifyAdmin, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('DELETE operation not suported on /imageUpload')
+    })
+
+module.exports = uploadRouter
