7c040b0b70f75974aee9ddd21cfd7d0de51f7c9c
M Dek <marek.deja@yahoo.com>
2020-08-26 19:01:23 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '311333563472135',
+        clientSecret: '433907cb12e3907338de3e865ceeeef2'
+    }
 }
